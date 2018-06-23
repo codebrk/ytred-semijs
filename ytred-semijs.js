f8bdd18da07e819660e3d8bb30b9f8b1f8aedf4a
codebrk <codebreakme@gmail.com>
2018-06-23 10:28:16 +1000

    deleted old messy code and rewritten.

--- ytred-semijs.js
+++ ytred-semijs.js
@@ -1,228 +1,126 @@
-function ytred(props) {
-	var totalMoves;
-	var originalMoves;
-	var leftEventBinded = false;
-	var rightEventBinded = true;
+$.plugin(function (prop) {
+	_self = this;
 
-	function bindRightArrowEvent(leftArrow, rightArrow, wrap, parent) {
-		rightArrow.css({
-			opacity: "1"
-		});
-		rightArrow.bind("click", function() {
-			totalMoves--;
-			moveRight(leftArrow, rightArrow, wrap, parent);
-			if (totalMoves <= 0 && rightEventBinded) {
-				unBindRightArrowEvent(leftArrow, rightArrow, wrap, parent);
-				rightEventBinded = false;
+	function moveRight(wrap, dist) {
+		console.log(dist);
+		console.log(wrap.offsetLeft());
+		console.log(wrap.offsetLeft() - dist);
+		wrap.css({ left: wrap.offsetLeft() - dist + "px" });
+	}
+
+	function moveLeft(wrap, dist) {
+		console.log(wrap.offsetLeft() + dist);
+		wrap.css({ left: wrap.offsetLeft() + dist + "px" });
+	}
+
+	function showHideArrows(leftArrow, rightArrow, moves, totalMoves) {
+		if (moves === 0) {
+			rightArrow.hide();
+		} else {
+			rightArrow.show();
+		}
+
+		if (moves === totalMoves) {
+			leftArrow.hide();
+		} else {
+			leftArrow.show();
+		}
+	}
+
+	function initialize() {
+		_self.each(function(el, i) {
+			el = $(el);
+			var items = el.children().filter(".ytred-item");
+			var wrap = $("<div>").append(items).addClass("ytred-wrap");
+			el.append(wrap);
+
+			var totalItems = items.length;
+			var itemsCanFit = 0;
+			var itemWidth = 205;
+			var containerWidth = el.width();
+			var moves = 0;
+
+			while (true) {
+				if (containerWidth < itemWidth) {
+					break;
+				}
+
+				itemsCanFit++;
+				containerWidth -= itemWidth;
 			}
 
-			if (totalMoves < originalMoves && !leftEventBinded) {
-				bindLeftArrowEvent(leftArrow, rightArrow, wrap, parent);
-				leftEventBinded = true;
+			while (true) {
+				if (totalItems <= itemsCanFit) {
+					break;
+				}
+
+				moves++;
+				totalItems -= itemsCanFit;
 			}
-		});
-	}
 
-	function unBindRightArrowEvent(leftArrow, rightArrow, wrap, parent) {
-		rightArrow.css({
-			opacity: "0"
-		});
-		rightArrow.unbind("click");
-	}
+			var totalMoves = moves;
 
+			// set background images
+			items.each(function(item, i) {
+				item = $(item);
+				item.css({
+					background: "url(" + item.attr("data-image") + ")",
+					backgroundSize: "cover",
+					backgroundPosition: "center",
+					width: (el.width() / itemsCanFit) - 5 + "px"
+				});
 
-	function bindLeftArrowEvent(leftArrow, rightArrow, wrap, parent) {
-		leftArrow.css({
-			opacity: "1"
-		});
-		leftArrow.bind("click", function() {
-			totalMoves++;
-			moveLeft(leftArrow, rightArrow, wrap, parent);
-			if (totalMoves >= originalMoves && leftEventBinded) {
-				unBindLeftArrowEvent(leftArrow, rightArrow, wrap, parent);
-				leftEventBinded = false;
-			}
-			console.log(totalMoves);
-			if (totalMoves <= 1 && !rightEventBinded) {
-				bindRightArrowEvent(leftArrow, rightArrow, wrap, parent);
-				rightEventBinded = true;
-			}
-		});
-	}
+				item.unbind("mouseover");
+				item.bind("mouseover", function() {
+					
+				});
 
-	function unBindLeftArrowEvent(leftArrow, rightArrow, wrap, parent) {
-		leftArrow.css({
-			opacity: "0"
-		});
-		leftArrow.unbind("click");
-	}
-
-	function hideArrows(leftArrow, rightArrow, wrap, parent, moves) {
-		totalMoves = moves;
-		originalMoves = moves;
-		bindRightArrowEvent(leftArrow, rightArrow, wrap, parent);
-		leftArrow.css({
-			opacity: "0"
-		});
-	}
-
-
-	function moveRight(leftArrow, rightArrow, wrap, parent) {
-		var destPos = (wrap[0].offsetLeft) + -(parent.width());
-
-		function move() {
-			var requestId = requestAnimFrame(move, wrap);
-
-			wrap.css({
-				left: destPos + "px",
+				item.unbind("mouseout");
+				item.bind("mouseout", function() {
+					
+				});
 			});
-		}
-		move();
-	}
 
-	function moveLeft(leftArrow, rightArrow, wrap, parent) {
-		var destPos = (wrap[0].offsetLeft) + (parent.width());
+			// adding title section
+			var titleContainer = $("<div>").addClass("ytred-title-container");
+			var title = $("<div>").addClass("ytred-title").html(el.attr("data-title"))
+				.css({ color: prop });
+			var leftArrow = $("<div>").addClass("ytred-left-arrow")
+				.html('<svg style="width: 24px; height: 24px;"><g mirror-in-rtl=""><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z" style="fill: ' + prop + ';"></path></g></svg>');
+			var rightArrow = $("<div>").addClass("ytred-right-arrow")
+				.html('<svg style="width: 24px; height: 24px;"><g mirror-in-rtl="" style="fill: ' + prop + ';"><path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"></path></g></svg>');
+			var arrowContainer = $("<div>").addClass("ytred-arrow-container")
+				.append(leftArrow).append(rightArrow);
+
+			titleContainer.append(title).append(arrowContainer);
+			el.prepend(titleContainer);
 
 
-		function move() {
-			var requestId = requestAnimFrame(move, wrap);
+			rightArrow.unbind("click");
+			rightArrow.bind("click", function() {
+				if (moves > 0) {
+					moveRight(wrap, el.width());
+					moves--;
+				}
 
-			wrap.css({
-				left: destPos + "px",
+				showHideArrows(leftArrow, rightArrow, moves, totalMoves);
 			});
-		}
-		if (Math.abs(wrap[0].offsetLeft) > 0) {
-			move();
-		}
+
+			leftArrow.unbind("click");
+			leftArrow.bind("click", function() {
+				if (moves < totalMoves) {
+					moveLeft(wrap, el.width());
+					moves++;
+				}
+
+				showHideArrows(leftArrow, rightArrow, moves, totalMoves);
+			});
+
+			moveRight(wrap, el.width());
+			moveLeft(wrap, el.width());
+			showHideArrows(leftArrow, rightArrow, moves, totalMoves);
+		});
 	}
 
-	function initialize(leftArrow, rightArrow, arrowContainer, titleContainer, title, wrap, parent, items) {
-		arrowContainer.css({
-			display: "flex"
-		});
-
-		leftArrow.css({
-			width: "24px",
-			height: "24px",
-			padding: "5px",
-			cursor: "pointer"
-		});
-		rightArrow.css({
-			width: "24px",
-			height: "24px",
-			padding: "5px",
-			cursor: "pointer"
-		});
-
-		titleContainer.css({
-			display: "flex",
-			overflow: "hidden",
-			justifyContent: "space-between",
-			paddingBottom: "20px",
-			alignItems: "center"
-		});
-
-		title.css({
-			color: props.fg,
-			fontSize: "18px"
-		});
-
-		parent.css({
-			width: "100%",
-			position: "relative",
-			height: "313px",
-			overflow: "hidden"
-		});
-
-		wrap.css({
-			position: "relative",
-			transition: "all .2s",
-			float: "left"
-		});
-
-
-		var count = 0;
-		var moves = 0;
-		var totalItems = items.length;
-		var containerWidth = parent.width();
-
-		while (true) {
-			if (containerWidth < 200) {
-				break;
-			}
-
-			containerWidth -= 200;
-			count++;
-		}
-
-		while (true) {
-			totalItems -= count;
-			moves++;
-
-			if (totalItems <= count) {
-				break;
-			}
-		}
-
-
-		
-		items.css(function(el, i) {
-			var leftPos = (parent.width() / count * i);
-			return {
-				height: "313px",
-				width: parent.width() / count + "px",
-				background: "url(" + $(el).attr("data-image") + ")",
-				backgroundRepeat: "no-repeat",
-				backgroundPosition: "center",
-				backgroundSize: "cover",
-				display: "inline-block",
-				position: "absolute",
-				left: leftPos + "px",
-				boxSizing: "border-box",
-				borderRight: "5px solid " + props.bg
-			}
-		});
-
-
-		hideArrows(leftArrow, rightArrow, wrap, parent, moves);
-		moveRight(leftArrow, rightArrow, wrap, parent);
-		moveLeft(leftArrow, rightArrow, wrap, parent);
-	}
-	var isResizeEventAdded = false;
-	this.each(function(parent) {
-		var items = $(parent.getElementsByClassName('ytred-item'));
-		parent = $(parent);
-		var wrap = items.wrap();
-		items.remove();
-		parent.append(wrap);
-
-		var titleContainer = $(document.createElement("div"));
-		var title = $(document.createElement("div"));
-		title.text(parent.attr("data-title"));
-		var leftArrow = $(document.createElement("div"));
-		leftArrow.html('<svg style="width: 24px; height: 24px;"><g mirror-in-rtl=""><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z" style="fill: ' + props.fg + ';"></path></g></svg>');
-		var rightArrow = $(document.createElement("div"));
-		rightArrow.html('<svg style="width: 24px; height: 24px;"><g mirror-in-rtl="" style="fill: ' + props.fg + ';"><path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"></path></g></svg>');
-		var arrowContainer = $(document.createElement("div"));
-
-		arrowContainer.append(leftArrow);
-		arrowContainer.append(rightArrow);
-		titleContainer.append(title);
-		titleContainer.append(arrowContainer);
-		parent.prepend(titleContainer);
-
-
-		initialize(leftArrow, rightArrow, arrowContainer, titleContainer, title, wrap, parent, items);
-
-		if (!isResizeEventAdded) {
-			window.addEventListener("resize", function() {
-				initialize(leftArrow, rightArrow, arrowContainer, titleContainer, title, wrap, parent, items);
-				console.log(wrap[0].offsetLeft);
-			}, false);
-			isResizeEventAdded = true;
-		}
-	});
-}
-
-
-$.plugin(ytred);
+	initialize();
+}, "ytred");
